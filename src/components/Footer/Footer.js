3dbd21bb8d8e57165a8df81102228f1542d63e5b
jorgeAgoiz <jorgeagoiz@gmail.com>
2021-05-27 08:47:59 +0200

    Linkedin añadido

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className="ui vertical footer segment padded">
             <div className="ui center aligned container">
-                <a href="#">
+                <a href="https://www.linkedin.com/in/jorge-agoiz-pedraja-78321b39/">
                     <img className="ui centered mini image" src="https://image.flaticon.com/icons/png/512/174/174857.png"></img>
                 </a>
                 <div className="ui divider"></div>
